e9fa02723ac29d1e80a202318857ee3843c96e7e
belevgen <belsky_evgeniy@mail.rum>
2022-06-18 15:58:00 +0600

    ДЗ Семинар 3_13.06.22_Задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите число => ");
+string Pal = Console.ReadLine();
+int Len = Pal.Length;
+
+bool q = true;
+
+for (int i = 0; i <= Len / 2; i++)
+{
+    if (Pal[i] != Pal[Len - 1 - i])
+    {
+        Console.WriteLine($"{Pal} Не палиндром");
+        q = false;
+        break;
+    }
+}
+
+if (q == true)
+{
+    Console.WriteLine($"{Pal} Палиндром");
+}
