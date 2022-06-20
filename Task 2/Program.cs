a29ea590873be251bb04681abaf2190ab06a1d0a
belevgen <belsky_evgeniy@mail.rum>
2022-06-20 21:29:55 +0600

    ДЗ Семинар 3_13.06.22_Задача 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+// ними в 3D пространстве.
+
+int prompt(string message)
+{
+    Console.WriteLine(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+double Sqr(double Num)
+{
+    return Num * Num;
+}
+
+int x1 = prompt("Введите x1");
+int y1 = prompt("Введите y1");
+int z1 = prompt("Введите z1");
+int x2 = prompt("Введите x2");
+int y2 = prompt("Введите y2");
+int z2 = prompt("Введите z2");
+
+double Dist(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double S = Sqr(x2 - x1) + Sqr(y1 - y1) + Sqr(z2 - z1);
+    double result = Math.Sqrt(S);
+    return result;
+}
+
+Console.WriteLine($"Расстояние между точками составляет {Dist(x1, y1, z1, x2, y2, z2)}");
